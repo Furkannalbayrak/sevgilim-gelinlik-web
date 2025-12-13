3d3e103121386114c53d940d7998f2c7db84fc73
Furkan Albayrak <furkanalb42@gmail.com>
2025-12-13 11:47:18 +0300

    app/gelinlik-modelleri yonlendirmesi

---
+++ app/gelinlik-modelleri/page.tsx
@@ -0,0 +1,9 @@
+import ProductList from "@/components/products/ProductList";
+
+export default function gelinlikModelleriPage(){
+    return(
+        <div>
+            <ProductList />
+        </div>
+    )
+}
