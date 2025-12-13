3df16ca7469f73ece70b9b363d39b75dec9eb728
Furkan Albayrak <furkanalb42@gmail.com>
2025-12-13 11:50:38 +0300

    yeni resimler icin tanimlama yapildi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,50 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.yesimgelinlik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-europe.dugunbuketi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "alissenuera.com",
+      },
+      {
+        protocol: "https",
+        hostname: "janroz.com.tr",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dsmcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.narinmoda.com",
+      },
+      {
+        protocol: "https",
+        hostname: "medihacambaz.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.davetcokelbisemyok.com",
+      },
+      {
+        protocol: "https",
+        hostname: "duguntrendy.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
