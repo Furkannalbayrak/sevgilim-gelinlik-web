fa6af614d2ddcc59d72369bf14ea2bed0fb9f187
Furkan Albayrak <furkanalb42@gmail.com>
2026-02-05 21:58:57 +0300

    Yeni kutuphane eklendi

--- next.config.ts
+++ next.config.ts
@@ -4,44 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "www.yesimgelinlik.com",
+        protocol: 'https',
+        hostname: 'cdn.dsmcdn.com',
       },
       {
-        protocol: "https",
-        hostname: "cdn-europe.dugunbuketi.com",
-      },
-      {
-        protocol: "https",
-        hostname: "alissenuera.com",
-      },
-      {
-        protocol: "https",
-        hostname: "janroz.com.tr",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.dsmcdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.narinmoda.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medihacambaz.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.davetcokelbisemyok.com",
-      },
-      {
-        protocol: "https",
-        hostname: "duguntrendy.com",
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
       },
     ],
   },
